01204c9543f935d2911cd938952561f9ee3f58ac
Edu Rivas <erivas17@gmail.com>
2022-08-04 15:01:37 -0400

    firebase setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCk77RWF1pw5VahrSYJ1LvAbaomaG_0cg4",
+    authDomain: "mbti-6e9df.firebaseapp.com",
+    databaseURL: "https://mbti-6e9df-default-rtdb.firebaseio.com",
+    projectId: "mbti-6e9df",
+    storageBucket: "mbti-6e9df.appspot.com",
+    messagingSenderId: "532325850985",
+    appId: "1:532325850985:web:1d14ae617ea7a123a167f8",
+    measurementId: "G-HCV7J9LP6Y"
+  }
 };
